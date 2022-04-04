5dfd49f06993a751d2fbf9b01ae1c1484ab5cb74
alex2534 <alex2534@outlook.com>
2022-04-04 08:23:20 -0300

    Portal

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,6 @@
+const Wrapper = (props) => {
+	// the props.children holds all the contente between the open and closeing tag of the constme componente
+	return props.children;
+};
+
+export default Wrapper;
